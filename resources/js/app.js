39c3df96ea19cd888e2d889589b4a6c05a525cde
mayoumelon <mayoumelon@gmail.com>
2021-05-24 06:27:58 +0900

    Vueルーティングまで

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
 const app = new Vue({
   el: '#app',
-  data: {
-    message: 'こんにちは'
-  },
-  template: '<h1>Hello world</h1>'
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
 })
