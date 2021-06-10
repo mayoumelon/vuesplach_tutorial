ef0db0e38852a5b64127759ca12676118d4d9536
mayoumelon <mayoumelon@gmail.com>
2021-06-11 06:32:24 +0900

    User API

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,16 @@ import './bootstrap'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む
-  store,  // ストアを読み込む
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+
+  new Vue({
+    el: '#app',
+    router, // ルーティングの定義を読み込む
+    store,  // ストアを読み込む
+    components: { App }, // ルートコンポーネントの使用を宣言する
+    template: '<App />' // ルートコンポーネントを描画する
+  })
+}
+
+createApp()
